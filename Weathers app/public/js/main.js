c9c3f6eb72fe1dfdc1cc475be324057e9240fa26
Afroz <mohdafroz.712042.co@mhssce.ac.in>
2023-06-08 17:53:40 +0530

    main.js

--- Weathers app/public/js/main.js
+++ Weathers app/public/js/main.js
@@ -6,7 +6,6 @@ const temp_value = document.getElementById('temp_value')
 const day_now = document.getElementById('day_now')
 const time_now = document.getElementById('time_now')
 const temp_text = document.getElementById('temp_text')
-// const getDateInfo = require('../js/info');
 
 const getInfo = async (event) => {
     event.preventDefault();
@@ -16,18 +15,13 @@ const getInfo = async (event) => {
     }
     else {
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=d9abf0799f002ae3577d242799eb5ad7`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid={apikeyhere}`
             const weather_data = await fetch(url);
             const jso = await weather_data.json();
             const arrData = [jso];
             city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`
             temp_value.innerText = arrData[0].main.temp;
             temp_status.innerText = arrData[0].weather[0].main;
-
-            // const city = `${arrData[0].name}`; // Replace 'Your City' with a default city, e.g., 'New York'
-            // const { date_y, day_y, time_y} = getDateInfo(city);
-
-            // time_now.innerText = time_y
 
             const temp_temp_status = arrData[0].weather[0].main;
             if (temp_temp_status == "Clear") {
